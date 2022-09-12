4a2031a95099fabbd56703c3999a92a15f9c391a
Catarina Belém <PastelBelem8@users.noreply.github.com>
2022-09-11 18:29:48 -0700

    Update GEM-SUMM-eval-baselines-3b.sh

--- bin/GEM-SUMM-eval-baselines-3b.sh
+++ bin/GEM-SUMM-eval-baselines-3b.sh
@@ -29,53 +29,23 @@ do
     for dataset in realsumm
     do
         export OUTPUT_PATH=$NICL_ROOT/exp_out${EXP_DESC}/${dataset}/${n_classes}class
-        export orig_exp_name=${model}_${dataset}_baseline
+        orig_exp_name=${model}_${dataset}_baseline
         python -m src.pl_eval -c ${model}.json+${dataset}.json -k dataset_classes=${n_classes} exp_name=${orig_exp_name} save_model=False
-    done
+    
 
 
-    # ------------------------------------------------------------------------------
-    # Baseline 2: Fine tune TO on training set (w/ IA3)
-    # ------------------------------------------------------------------------------
-    # Evaluate Models in a zero shot (no intermediate pretraining). It is a lower
-    # bound and gives us an idea of how much we have to improve.
-    # ------------------------------------------------------------------------------
-    echo "Fine tuning ${model} w/ (IA)3"
-    for dataset in realsumm
-    do
-        export OUTPUT_PATH=$NICL_ROOT/exp_out${EXP_DESC}/${dataset}/${n_classes}class
-        export orig_exp_name=${model}_${dataset}_baseline_ft_train
+        # ------------------------------------------------------------------------------
+        # Baseline 2: Fine tune TO on training set (w/ IA3)
+        # ------------------------------------------------------------------------------
+        # Evaluate Models in a zero shot (no intermediate pretraining). It is a lower
+        # bound and gives us an idea of how much we have to improve.
+        # ------------------------------------------------------------------------------
+        echo "Fine tuning ${model} w/ (IA)3"
+        orig_exp_name=${model}_${dataset}_baseline_ft_train
         python -m src.pl_train -c ${model}.json+ia3.json+${dataset}.json -k load_weight="pretrained_checkpoints/${model}_ia3_finish.pt" save_model=True dataset_classes=${n_classes} exp_name=${orig_exp_name}
-    done
 
-    echo "Fine tuning ${model} w/ (IA)3 for 10k steps"
-    for dataset in realsumm
-    do
-        export OUTPUT_PATH=$NICL_ROOT/exp_out${EXP_DESC}/${dataset}/${n_classes}class
-        export orig_exp_name=${model}_${dataset}_baseline_ft_train_10k_steps
+        echo "Fine tuning ${model} w/ (IA)3 for 10k steps"
+        orig_exp_name=${model}_${dataset}_baseline_ft_train_10k_steps
         python -m src.pl_train -c ${model}.json+ia3.json+${dataset}.json -k load_weight="pretrained_checkpoints/${model}_ia3_finish.pt" save_model=True dataset_classes=${n_classes} exp_name=${orig_exp_name} num_steps=10000
-    done
-
-
-    # ------------------------------------------------------------------------------
-    # Baseline 3: Fine tune TO on all sets (w/ IA3)
-    # ------------------------------------------------------------------------------
-    # Evaluate Models in a zero shot (no intermediate pretraining). It is a lower
-    # bound and gives us an idea of how much we have to improve.
-    # ------------------------------------------------------------------------------
-    echo "Fine tuning ${model} w/ (IA)3 on all data"
-    for dataset in realsumm
-    do
-        export OUTPUT_PATH=$NICL_ROOT/exp_out${EXP_DESC}/${dataset}/${n_classes}class
-        export orig_exp_name=${model}_${dataset}_baseline_ft_all
-        python -m src.pl_train -c ${model}.json+ia3.json+${dataset}.json -k load_weight="pretrained_checkpoints/${model}_ia3_finish.pt" fine_tune_with_all=True save_model=true dataset_classes=${n_classes} exp_name=${orig_exp_name} few_shot=False
-    done
-
-    echo "Fine tuning ${model} w/ (IA)3 on all data (num_steps=20k)"
-    for dataset in realsumm
-    do
-        export OUTPUT_PATH=$NICL_ROOT/exp_out${EXP_DESC}/${dataset}/${n_classes}class
-        export orig_exp_name=${model}_${dataset}_baseline_ft_all_10k_steps
-        python -m src.pl_train -c ${model}.json+ia3.json+${dataset}.json -k load_weight="pretrained_checkpoints/${model}_ia3_finish.pt" fine_tune_with_all=True save_model=true dataset_classes=${n_classes} exp_name=${orig_exp_name} few_shot=False num_steps=10000
     done
 done
