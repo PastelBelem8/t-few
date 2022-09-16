785b4e2494390c8a2759e4a2887dac2580d394a1
Catarina Belém <PastelBelem8@users.noreply.github.com>
2022-09-16 07:28:25 -0700

    Update data_module.py

--- src/data/data_module.py
+++ src/data/data_module.py
@@ -3,6 +3,30 @@ import numpy as np
 from pytorch_lightning import LightningDataModule
 
 
+def encode_and_truncate(input_str, add_special_tokens, tokenizer):
+    output = tokenizer(
+        input_str,
+        return_tensors="pt",
+        add_special_tokens=add_special_tokens,
+        # Note: overflowing tokens requires adding truncation and padding
+        return_overflowing_tokens=True, 
+        truncation=True, 
+        padding="max_length",
+    )
+
+    overflow_map = output["overflow_to_sample_mapping"].tolist()
+
+    keep_ids = np.unique(overflow_map) 
+    trunc_ids = [i for i in range(len(overflow_map)) if i not in keep_ids]
+    
+    input_ids = output["input_ids"]
+    num_truncated = (
+        input_ids[trunc_ids,:] != tokenizer.pad_token_id
+    ).sum().item()
+
+    return input_ids.squeeze(0), num_truncated
+
+
 class FinetuneDataModule(LightningDataModule):
     def __init__(self, config, tokenizer, dataset_reader):
         super().__init__()
@@ -25,7 +49,7 @@ class FinetuneDataModule(LightningDataModule):
             self.train_dataset = self.dataset_reader.read_orig_dataset("train")
         self.dev_dataset = self.dataset_reader.read_orig_dataset("validation") # Dataset
 
-        extra_args = {} ##{"add_special_tokens": False} ## added on 12 sep 
+        extra_args = {"add_special_tokens": self.config.add_special_tokens}
         self.train_dataset = FinetuneDatasetWithTemplate(
             self.train_dataset, self.dataset_reader.get_train_template(), self.tokenizer, **extra_args
         )
@@ -76,35 +100,28 @@ class FinetuneDatasetWithTemplate(torch.utils.data.dataset.Dataset):
         
         answer_choices = template.get_answer_choices_list(example)
         if isinstance(input_str, list):
-            input_ids = torch.cat(
-                [
-                    self.tokenizer(
-                        input_field, return_tensors="pt", truncation=True, add_special_tokens=False
-                    ).input_ids.squeeze(0)
-                    for input_field in input_str[:-1]
-                ]
-                + [
-                    self.tokenizer(
-                        input_str[-1], return_tensors="pt", truncation=True, add_special_tokens=self.add_special_tokens
-                    ).input_ids.squeeze(0)
-                ]
-            )
+            encodings = (encode_and_truncate(input_field, False, self.tokenizer) for input_field in input_str[:-1])
+            encodings_input, num_truncated = zip(*encodings)
+            encoding_last, num_truncated_last = encode_and_truncate(input_str[-1], self.add_special_tokens, self.tokenizer) 
+
+            input_ids = torch.cat(encodings_input, encoding_last)
+            num_truncated = sum(num_truncated) + num_truncated_last
         else:
-            input_ids = self.tokenizer(
-                input_str, return_tensors="pt", truncation=True, add_special_tokens=self.add_special_tokens
-            ).input_ids.squeeze(0)
-        target_ids = self.tokenizer(
-            target_str, return_tensors="pt", truncation=True, add_special_tokens=self.add_special_tokens
-        ).input_ids.squeeze(0)
+            input_ids, num_truncated = encode_and_truncate(
+                input_str, self.add_special_tokens, self.tokenizer)
+        
+        # We assume the targets are usually small and therefore we will not
+        # consider they truncation.
+        target_ids, _ = encode_and_truncate(
+            target_str, add_special_tokens=self.add_special_tokens, tokenizer=self.tokenizer
+        )
         answer_choices_ids = [
-            self.tokenizer(
-                answer_choice, return_tensors="pt", truncation=True, add_special_tokens=self.add_special_tokens
-            ).input_ids.squeeze(0)
+            encode_and_truncate(answer_choice, self.add_special_tokens, self.tokenizer)[0]
             for answer_choice in answer_choices
         ]
         label = torch.LongTensor([example["label"]])
         idx = torch.LongTensor([example["idx"]])
-        return input_ids, target_ids, answer_choices_ids, label, idx
+        return input_ids, target_ids, answer_choices_ids, label, idx, torch.LongTensor([num_truncated])
 
 
 class PretrainDataModule(LightningDataModule):
@@ -168,13 +185,21 @@ class PretrainDatasetWithTemplate(torch.utils.data.dataset.Dataset):
 def create_collate_fn(pad_token_id, pretrain):
     def collate_fn(batch):
         if not pretrain:
-            input_ids, target_ids, answer_choices_ids, labels, idx = zip(*batch)
+            input_ids, target_ids, answer_choices_ids, labels, idx, num_truncated = zip(*batch)
         else:
             input_ids, target_ids = zip(*batch)
 
+        # Update 2022-08-14 @pastelbelem8
+        # ---------------------------------------------------------------------
+        # In an attempt to know exactly how many sequences are being truncated
+        # and how its related with the model's performance.
+        # ---------------------------------------------------------------------
+        seq_lens = [len(b[0]) for b in batch]
+        # ---------------------------------------------------------------------
         input_ids = torch.nn.utils.rnn.pad_sequence(input_ids, batch_first=True, padding_value=pad_token_id)
         target_ids = torch.nn.utils.rnn.pad_sequence(target_ids, batch_first=True, padding_value=pad_token_id)
         output_batch = {
+            "input_seq_len": seq_lens,
             "input_ids": input_ids,
             "target_ids": target_ids,
         }
@@ -195,6 +220,7 @@ def create_collate_fn(pad_token_id, pretrain):
                     "answer_choices_ids": answer_choices_ids,
                     "labels": labels,
                     "idx": idx,
+                    "num_truncated": num_truncated,
                 }
             )
         return output_batch
@@ -220,15 +246,17 @@ class EvalDataModule(LightningDataModule):
     def setup(self, stage):
         self.test_dataset = self.dataset_reader.read_orig_dataset("test") # Dataset
 
+        extra_args = {"add_special_tokens": self.config.add_special_tokens}
+        
         self.test_dataset = FinetuneDatasetWithTemplate(
-            self.test_dataset, self.dataset_reader.get_eval_template(), self.tokenizer
+            self.test_dataset, self.dataset_reader.get_eval_template(), self.tokenizer, **extra_args
         )
         print(f"Test size {len(self.test_dataset)}")
 
         if not self.test_only:
             self.dev_dataset = self.dataset_reader.read_orig_dataset("validation") # Dataset
             self.dev_dataset = FinetuneDatasetWithTemplate(
-                self.dev_dataset, self.dataset_reader.get_eval_template(), self.tokenizer
+                self.dev_dataset, self.dataset_reader.get_eval_template(), self.tokenizer, **extra_args
             )
             print(f"Dev size {len(self.dev_dataset)}")
         
